6a2a47c07f05c328e39a127335fa9ffe5304b7cc
Madhuri1945 <madhuridhulipudi37@gmail.com>
2025-03-04 20:56:13 +0530

    Updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  optimizeDeps: {
-    include: ["swiper"],
-  },
+  plugins: [react()],
   build: {
-    outDir: "dist", // Keep the default output directory for built files
-    emptyOutDir: true, // Clean the output directory before building
-  },
-  publicDir: "public-assets", // Use a separate directory for static assets
-  server: {
-    port: 3000, // Optional: Customize the development server port
+    outDir: "dist",
   },
 });
